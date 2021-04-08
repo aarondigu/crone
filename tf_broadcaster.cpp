c6b8488a85bd6c6845b95bf1c175208c7a3b8e5f
aarondigu <aarondigu@gmail.com>
2021-04-07 19:50:38 -0500

    tf_broadcaster updated

--- tf_broadcaster.cpp
+++ tf_broadcaster.cpp
@@ -10,20 +10,20 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <std_msgs/Float64.h>
+
 
 
 geometry_msgs::PoseStamped current_pose;
+
 void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
-
   current_pose = *msg;
+}
 
-  static tf::TransformBroadcaster br1;
-  tf::Transform transform1;
-  transform1.setOrigin( tf::Vector3(current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z) );
-  // q: x, y, z, w
-  // roll (X), pitch (Y), yaw (Z), from parent to child in order: yaw -> pitch -> roll
-  transform1.setRotation(tf::Quaternion(current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w) );
-  br1.sendTransform(tf::StampedTransform(transform1, ros::Time::now(), "map", "base_link"));
+std_msgs::Float64 current_alt;
+
+void alt_cb(const std_msgs::Float64::ConstPtr& msg){
+  current_alt = *msg;
 }
 
 int main(int argc, char** argv){
@@ -33,7 +33,23 @@ int main(int argc, char** argv){
   ros::NodeHandle node;
   ros::Subscriber pose_sub = node.subscribe<geometry_msgs::PoseStamped>
             ("mavros/local_position/pose", 10, pose_cb);
+  ros::Subscriber alt_sub = node.subscribe<std_msgs::Float64>
+            ("mavros/global_position/rel_alt", 10, alt_cb);
   ros::Rate rate(10.0);
+
+  //Initializing variables
+  current_pose.pose.position.x = 0.0;
+  current_pose.pose.position.y = 0.0;
+  current_pose.pose.position.z = 0.0;
+  current_pose.pose.orientation.x = 0.0;
+  current_pose.pose.orientation.y = 0.0;
+  current_pose.pose.orientation.z = 0.0;
+  current_pose.pose.orientation.w = 1.0;
+  current_alt.data = 0.0;
+
+  static tf::TransformBroadcaster br1;
+  tf::Transform transform1;
+
 
   static tf::TransformBroadcaster br;
   tf::Transform transform;
@@ -44,11 +60,16 @@ int main(int argc, char** argv){
   //q.setRPY(1.5708, 0.0, -1.5708);
   q.setRPY(-1.5708, 0.0, -1.5708);
   transform.setRotation(q);
-  //transform.setRPY(1.5708, -1.5708, 0.0);
-  //FOR SIMULATED RGBD CAMERA:
 
   ROS_INFO("Broadcasting tf transforms...");
   while(ros::ok()){
+
+  transform1.setOrigin( tf::Vector3(current_pose.pose.position.x, current_pose.pose.position.y, current_alt.data) );
+  // q: x, y, z, w
+  // roll (X), pitch (Y), yaw (Z), from parent to child in order: yaw -> pitch -> roll
+  transform1.setRotation(tf::Quaternion(current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w) );
+  br1.sendTransform(tf::StampedTransform(transform1, ros::Time::now(), "map", "base_link"));
+
   br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "camera_link"));
   //FOR REALSENSE D435:
   br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "camera_depth_optical_frame"));
